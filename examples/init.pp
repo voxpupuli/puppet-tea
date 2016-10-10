8e8aeb42a86f034480fe1131ce1a3b4e9e6b7aa7
Tim Meusel <tim@bastelfreak.de>
2016-10-10 19:47:33 +0200

    puppet-lint: fix arrow_alignment relative_classname_inclusion

--- examples/init.pp
+++ examples/init.pp
@@ -1,8 +1,8 @@
 class tea {
 
-  class { "tea::ports":
-    any_port => 5001,
-    unp_port => 5002,
+  class { '::tea::ports':
+    any_port  => 5001,
+    unp_port  => 5002,
     priv_port => 1001,
   }
 
